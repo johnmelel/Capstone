c5222a39ede9a96fc89f9f5e6705f60ac97b41f5
John Melel <johnjojimelel@gmail.com>
2025-11-14 01:31:04 -0600

    deleted legacy codes

--- Legacy/architecture_v1/main.py
+++
@@ -1,9 +0,0 @@
-from manager import AgentManager
-from PIL import Image
-
-if __name__ == '__main__':
-    manager = AgentManager()
-    query = 'Find diabetic foot ulcer cases with MRI images'
-    example_img = Image.new('RGB', (224,224))
-    manager.run(query, [example_img])
-    print('Done')
